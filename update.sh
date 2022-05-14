5e08705ee2f247a1c4a03c3c3f4b8d91198a20e6
Program108 <66560463+Program108@users.noreply.github.com>
2022-05-15 00:54:40 +0530

    Delete update.sh

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rm index.js
-rm schemas.js
-cp /storage/emulated/0/AppProjects/Php/schemas.js $HOME/SchoolProject/
-cp /storage/emulated/0/AppProjects/Php/index.js $HOME/SchoolProject/
